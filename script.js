eab55eb08cb0ac57ca8f22fd61e128120b3fd0bf
rakshita ganesh <120761109+RakshitaGanesh22@users.noreply.github.com>
2023-02-17 07:44:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 //your code here
+function minCost(arr) {
+    for(var i=0;i<arr.length;i++){
+        for(var j=i;i<arr.length;j++){
+            if(arr[j]<arr[i]){
+                var temp=arr[i];
+                arr[j]=arr[i];
+                arr[j]=temp;
+            }
+        }
+       minCost1(arr);
+    }
+   }
+   function minCost1(arr){
+    if(arr.lengh>1){
+        arr[1]=arr[0]+arr[1];
+        arr.shift();
+        minCost(arr);
+    }
+    else{
+        console.log(arr[0]);
+		return;
+    }
+   }
+   
+   /*Do not change the code below */
+   
+   var readline = require("readline").createInterface(process.stdin);
+   
+   let inputArr = [];
+   var lineNumber = -1;
+   var inputSize;
+   
+   //multipleline input from User
+   readline.on("line", readInputs);
+   
+   function readInputs(line) {
+     inputArr.push(line);
+     lineNumber++;
+     //Exit Condition
+     if (lineNumber == 0) {
+       logic("s" );
+       readline.close();
+       
+     }
+   }
+   
+   function logic(input) {
+     let str = JSON.parse(inputArr[0].trim());
+     minCost(str);
+   }
+   
